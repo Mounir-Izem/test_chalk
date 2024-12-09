341899ed72e4db2f318c19e9346b1463f4c812d8
Mounir Izem <izem.mounir@gmail.com>
2024-12-09 10:51:50 +0100

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+const theo = "Théophile";
+const kev = "Kévin";
+const clarel = "Clarelle";
+const clem = "Clément";
+
+console.log(chalk.blue(theo) + " " + chalk.green(kev) + " " + chalk.yellow(clarel) + " " + chalk.green(clem));
